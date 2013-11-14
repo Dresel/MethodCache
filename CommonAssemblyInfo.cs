da403328c9e6809fea92d17b010e4a0ad5f589f9
Christopher Dresel <christopher.dresel@fhwn.ac.at>
2013-11-14 11:43:47 +0100

    Wipe Project

--- CommonAssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("MethodCache.Fody")]
-[assembly: AssemblyProduct("MethodCache.Fody")]
-[assembly: AssemblyVersion("1.3.1.0")]
-[assembly: AssemblyFileVersion("1.3.1.0")]
--- MethodCache.TestAssembly/MethodResult.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace MethodCache.TestAssembly
-{
-	public class MethodResult
-	{
-	}
-}
