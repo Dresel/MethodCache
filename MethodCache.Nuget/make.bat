39516134a453add8936ad947eb9e1e32ea9e395b
Christopher Dresel <christopher.dresel@spatial-focus.net>
2016-04-12 17:27:54 +0200

    Remove net40 NuGet folder

--- MethodCache.Nuget/make.bat
+++ MethodCache.Nuget/make.bat
@@ -1,8 +1,8 @@
-mkdir input\lib\net40
-del /Q input\lib\net40\*.*
+mkdir input\lib\
+del /Q input\lib\*.*
 
-msbuild ..\MethodCache.Attributes\MethodCache.Attributes.csproj /p:Configuration=Release;OutputPath=..\MethodCache.Nuget\input\lib\net40
-msbuild ..\MethodCache.Fody\MethodCache.Fody.csproj /p:Configuration=Release;OutputPath=..\MethodCache.Nuget\input\lib\net40
+msbuild ..\MethodCache.Attributes\MethodCache.Attributes.csproj /p:Configuration=Release;OutputPath=..\MethodCache.Nuget\input\lib
+msbuild ..\MethodCache.Fody\MethodCache.Fody.csproj /p:Configuration=Release;OutputPath=..\MethodCache.Nuget\input\lib
 
 mkdir output
 ..\.nuget\nuget.exe pack /o output .\MethodCache.nuspec
